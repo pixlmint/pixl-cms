07f74784360327ce6ca5897ceeb47c9c28e1755b
ChristianGroeber <swiss8oy.chg@gmail.com>
2023-11-10 21:41:51 +0100

    removed composer.lock

--- config/base.php
+++ config/base.php
@@ -4,6 +4,6 @@ return [
     'mediaDir' => $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'media',
     'contentDir' => $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'content',
     'mediaBaseUrl' => DIRECTORY_SEPARATOR . 'media',
-    'version' => "2.0.1",
+    'version' => "2.0.2",
     'debugEnabled' => false,
 ];
