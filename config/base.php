3eb673c86e9f65de1963eb56d5a2ce8797a540f9
ChristianGroeber <swiss8oy.chg@gmail.com>
2024-11-19 12:54:40 +0100

    v2.3.0

--- config/base.php
+++ config/base.php
@@ -4,6 +4,6 @@ return [
     'mediaDir' => $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'media',
     'contentDir' => $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'content',
     'mediaBaseUrl' => DIRECTORY_SEPARATOR . 'media',
-    'version' => "2.2.1",
+    'version' => "2.3.0",
     'debugEnabled' => false,
 ];
